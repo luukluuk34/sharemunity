713af320e04bc79c4577dca23b5b8ba030d7ef84
Luuk Bartels <l.bartes1@student.avans.nl>
2024-03-05 16:28:20 +0100

    added firebase function

--- apps/data-api/src/main.ts
+++ apps/data-api/src/main.ts
@@ -10,6 +10,9 @@ import { AppModule } from './app/app.module';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import { ApiResponseInterceptor } from '@sharemunity-workspace/backend/dto';
 
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
@@ -19,11 +22,18 @@ async function bootstrap() {
   app.useGlobalInterceptors(new ApiResponseInterceptor);
 
 
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(
-    `Data API is running on: http://localhost:${port}/${globalPrefix}`
-  );
+  if(process.env.FUNCTIONS_EMULATOR){
+    const port = process.env.PORT || 3000;
+    await app.listen(port);
+    Logger.log(
+      `Data API is running on: http://localhost:${port}/${globalPrefix}`
+    );
+  } else {
+    const expressApp = express();
+    expressApp.use(`/${globalPrefix}`, new ExpressAdapter(app).getInstance());
+    module.exports.app = expressApp;
+  }
+
 }
 
 bootstrap();
